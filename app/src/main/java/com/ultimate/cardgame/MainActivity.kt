64953e7ee38f94276ae74928ebe035df2b67b69f
awpas50 <awper9794@gmail.com>
2026-09-15 21:36:41 +0800

    hide nav bar

--- app/src/main/java/com/ultimate/cardgame/MainActivity.kt
+++ app/src/main/java/com/ultimate/cardgame/MainActivity.kt
@@ -6,6 +6,9 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.addCallback
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : ComponentActivity() {
 
@@ -21,6 +24,12 @@ class MainActivity : ComponentActivity() {
             loadUrl(HOME_URL)
         }
         setContentView(webView)
+
+        // Hide the system navigation bar; swipe up from the bottom to reveal it temporarily
+        WindowCompat.getInsetsController(window, window.decorView).apply {
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            hide(WindowInsetsCompat.Type.navigationBars())
+        }
 
         onBackPressedDispatcher.addCallback(this) {
             if (webView.canGoBack()) {
